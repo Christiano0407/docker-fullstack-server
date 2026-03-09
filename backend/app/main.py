150469ed871e126acc80fb70b6460db39f47ac16
christiano0407 <placebo0704@gmail.com>
2026-03-09 13:18:30 -0600

    [Docker] Develop and crete new Images & Container to run the Project FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from app.services.data_product_workflow import load_data
 
 app = FastAPI()
+
+# = Debugging | para ver las rutas registradas
+print([route.path for route in app.routes])
 
 @app.get("/")
 def root():
   return{"message": "FastAPI running..."}
+
+@app.get("/data")
+def get_data(limit: int = 10):
+  """
+    API Endpoints | Return Data 
+  """
+  data = load_data(limit)
+  return {
+    "Count": len(data), 
+    "Data": data
+  }
--- backend/app/services/data_product_workflow.py
+++ backend/app/services/data_product_workflow.py
@@ -1,13 +1,18 @@
-from pathlib import Path
-import pandas as pd
 import csv
-
-BASE_DIR = Path(__file__).resolve.parent
-DATA_PATH = BASE_DIR / "../../data/disney_movies_2_cleaned.csv"
-
+from pathlib import Path
+from itertools import islice
 # df = pd.read_csv(DATA_PATH)
 
-def load_data():
+# __file__ = /app/app/services/data_product_workflow.py
+# .parent        → /app/app/services
+# .parent.parent → /app/app
+# .parent x3     → /app  ← WORKDIR donde está /data/
+# = "Data, por el momento no está en otro container" = #
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent 
+DATA_PATH = BASE_DIR / "data" / "disney_movies_2_cleaned.csv"# Up Two Levels #
+
+""" def load_data():
   rows = []
 
   with open(DATA_PATH, newline="") as f:
@@ -15,4 +20,21 @@ def load_data():
     for row in reader:
       rows.append(row)
 
+  return rows """
+
+def load_data(limit: int = 10):
+  """
+    Load Limit Market Data From CSV with a configurable row list.
+  """
+
+  if not DATA_PATH.exists():
+    raise FileNotFoundError(f"CSV not Fount: {DATA_PATH}")
+
+  with open(DATA_PATH, newline="", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+
+    # Return Only the "Limit" rows
+    rows = list(islice(reader, limit))
+
   return rows
+
