2b561fb899d2f01ed7eb5e3e1610d6b8ca8c9a68
christiano0407 <placebo0704@gmail.com>
2026-03-25 11:48:16 -0600

    [docker/frontend-app] Modify and add new features in frontend app Part I v7

--- frontend/src/components/MovieList.tsx
+++ frontend/src/components/MovieList.tsx
@@ -1,3 +1,11 @@
 /**
- * src/components/MovieList.tsx
+ * = src/components/MovieList.tsx = 
+ * - Cargar la lista de Movies - 
  */
+
+import { useEffect, useState } from "react";
+import { moviesAPI, type MovieListResponse } from "../api/moviesApi";
+import MovieCard from "./MovieCard";
+import Pagination from "./Pagination";
+
+const LIMIT = 10; 
--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -1,4 +1,40 @@
 /**
- * src/components/Pagination.tsx
+ * = src/components/Pagination.tsx =
+ * - Return 2 Pagination [Items / Páginas] -
  */
  
+interface Props {
+  offset: number; 
+  limit: number; 
+  total: number; 
+  onPrev: () => void; 
+  onNext: () => void; 
+}
+
+export default function Pagination({offset, limit, total, onPrev, onNext}: Props) {
+  const currentPage = Math.floor(offset / limit) + 1; 
+  const totalPages = Math.ceil(total / limit); 
+  
+  return (
+    <div id="idPagination" className="pagination">
+      <button
+        className="page-btn"
+        onClick={onPrev}
+        disabled={offset === 0}
+      >Prev</button>
+      
+      <p className="page-info">
+        {currentPage}
+        <span className="page-separation">/</span>
+        {totalPages}
+        <span className="page-total"> - {total} movies</span>
+      </p>
+
+      <button
+        className="page-btn"
+        onClick={onNext}
+        disabled={ offset + limit >= total}
+      >Next</button>
+    </div>
+  )
+}
