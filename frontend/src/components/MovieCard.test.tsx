355cb467b70ccff3555443cd096d95265c83a6ce
christiano0407 <placebo0704@gmail.com>
2026-03-26 12:51:51 -0600

    [docker/frontend-app] Modify and add new features in frontend app | Part III v1 Pipeline Test/vitest

--- frontend/src/components/MovieCard.test.tsx
+++ frontend/src/components/MovieCard.test.tsx
@@ -5,22 +5,22 @@
  * Verifica que el componente renderiza correctamente
  * con los datos de tu API. No llama a TMDB (mockeado).
  */
-import { describe, it, expect, vi, beforeEach } from "vitest";
+/* import { describe, it, expect, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import MovieCard from "../../src/components/MovieCard";
-import type { Movie } from "../../src/api/moviesApi";
+import type { Movie } from "../../src/api/moviesApi"; */
 
 // ── Mock de usePoster — no llamamos a TMDB en tests ──
 
 
 // ── Datos de prueba | Mocks  ───────────────────────────────────
-const MOCK_MOVIE: Movie = {
+/* const MOCK_MOVIE: Movie = {
   movie_title:    "The Lion King",
   release_date:   "15/06/1994",
   genre:          "Adventure",
   rating:         "G",
   total_gross:    422780140,
   adjusted_gross: 761640898,
-}
-
+} */
+/*  */
 // === Tests =============================================
