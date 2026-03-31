e6a6d8b573335f25d5caef5b04348c6c471460c7
christiano0407 <placebo0704@gmail.com>
2026-03-31 09:48:58 -0600

    [API] Add API TMDB & Get Fetch to add our Project. | Part 1: v1.1.1

---
+++ frontend/src/hooks/usePoster.ts
@@ -0,0 +1,7 @@
+/**
+ * src/hooks/usePoster.ts
+ * Hook que obtiene el poster de TMDB con caché en memoria.
+ *
+ * - Primera vez: llama a TMDB y guarda el resultado
+ * - Segunda vez: devuelve el resultado cacheado sin llamar de nuevo
+ */
